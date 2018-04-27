1762805dea8ee20e4075f5df22e96c77264e8a28
Andrey Pushkarev <fealaer@gmail.com>
2018-04-27 23:37:45 +0300

    Set up express hello world server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
-const sum = (a, b) => a + b;
+const config = require('config');
+const express = require('express');
 
-module.exports = sum;
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const { port } = config.server;
+app.listen(port, () => console.log(`Sever listening on port ${port}!`));
