a51764a8dbfa105e4b3bded19018a6579ebc0ce3
Andrey Pushkarev <fealaer@gmail.com>
2018-04-30 11:33:33 +0300

    Do not log not found errors with winston

--- server/helpers/handleError.js
+++ server/helpers/handleError.js
@@ -1,5 +1,6 @@
 const logger = require('../logger');
 const isValidationError = require('../helpers/isValidationError');
+const isNotFoundError = require('../helpers/isNotFoundError');
 
 const formatValidationDetails = error =>
   `${error.name}: ${error.message} - ${JSON.stringify(error.errors)}`;
@@ -13,8 +14,10 @@ const format = (error, details) => `${error.status} - ${error.code} - ${details}
 
 module.exports = (error) => {
   const err = error;
-  err.status = err.status || 500;
-  const level = chooseLevel(error);
-  logger[level](format(err, chooseDetails(error)));
+  if (!isNotFoundError(error)) {
+    err.status = err.status || 500;
+    const level = chooseLevel(error);
+    logger[level](format(err, chooseDetails(error)));
+  }
   return err;
 };
